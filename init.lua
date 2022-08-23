5b81a03d154c881956bdbcc39d0482dca24b6de9
Antnio Favarin Freire <favarinfreire@hotmail.com>
2022-08-23 10:43:24 -0300

    Adicionando novos arquivos

--- init.lua
+++ init.lua
@@ -1,14 +1,3 @@
 require('base')
 require('highlights')
 require('maps')
-
-local has = function(x) 
-  return vim.fn.has(x) == 1
-end
-
-
-local is_ubuntu = has "Ubuntu"
-
-if is_ubuntu then
-  require('linux')
-end
